566a99018aa8bf0276106397464030536f7415ad
Jose <jbangate@my.bcit.ca>
2022-05-09 18:11:06 -0700

    Added a new branch called sprint2 as well as Luca's work

--- Style/style.js
+++ Style/style.js
@@ -64,3 +64,11 @@ function Completed() {
   console.log(item)
 }
 
+const searchInput = document.querySelector("data-search")
+
+searchInput.addEventListener("input", (e) => {
+  const value = e.target.value
+  console.log(value)
+
+})
+
