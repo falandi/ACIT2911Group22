83fd0c9e7bcfded3e80f0e7df170e9cfd601af1e
Faith Alandi <faefaith@gmail.com>
2022-05-26 20:20:03 -0700

    final changes

--- Style/style.js
+++ Style/style.js
@@ -1,4 +1,5 @@
 function newElement() {
+<<<<<<< Updated upstream
   var button = document.createElement("button")
     button.setAttribute("class", "completeBtn")
     button.setAttribute("onClick", "Completed()")
@@ -35,6 +36,46 @@ function newElement() {
   li.appendChild(span)
 
   document.getElementById("myInput").value = ""
+=======
+    var button = document.createElement("button")
+      button.setAttribute("class", "completeBtn")
+      button.setAttribute("onClick", "Completed()")
+    var Remove = document.createElement("button")
+      Remove.setAttribute("class", "removeBtn")
+      Remove.setAttribute("onClick", "deleteConfirmation()")
+      Remove.setAttribute("padding", "5px")
+      Remove.textContent = "Remove"
+    var Edit = document.createElement("button")
+      Edit.setAttribute("class", "editBtn")
+      Edit.setAttribute("onClick", "Edit()")
+      Edit.textContent = "Edit"
+    var li = document.createElement("li")
+    var textarea = document.createElement("textarea")
+      textarea.setAttribute("rows","1")
+      textarea.setAttribute("cols","10")
+      textarea.setAttribute("class","to-do_text")
+    var date = document.createElement("input")
+      date.setAttribute("id", "date")
+      date.setAttribute("type", "datetime-local")
+      date.setAttribute("max", "2030-12-31")
+    var inputValue = document.getElementById("myInput").value;
+    var text = document.createTextNode(inputValue);
+    textarea.appendChild(text)
+    li.appendChild(button)
+    li.appendChild(textarea)
+    li.appendChild(date)
+
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("lItem").appendChild(li);
+    }
+    var span = document.createElement("SPAN")
+    span.className  = "close"
+    span.appendChild(Remove)
+    span.appendChild(Edit)
+    li.appendChild(span)
+>>>>>>> Stashed changes
 
 }
 
@@ -105,7 +146,31 @@ for (var i=0; i < li.length;i++) {
 
 }
 
+<<<<<<< Updated upstream
 }
 
 // module.exports = deleteConfirmation, Completed, Remove, newList, moveListItem
 
+=======
+const searchInput = document.querySelector("data-search")
+
+searchInput.addEventListener("input", (e) => {
+  const value = e.target.value
+  console.log(value)
+
+})
+
+function deleteConfirmation() {
+  let confirmation = "Are you sure you want to delete this task?";
+  if (confirm(confirmation) == true) {
+    Remove();
+  }
+}
+
+function moveListItem() {
+  toDelete = document.getElementById("lItem")
+  
+}
+
+module.exports = deleteConfirmation, Completed, Remove, newList, moveListItem
+>>>>>>> Stashed changes
